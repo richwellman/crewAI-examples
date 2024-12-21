1533098016ba3933eeb3adf8c718881b86cedaa7
Rich Wellman <richwellman@hotmail.com>
2024-12-21 21:43:41 +0000

    updated filepath for job description.  fixed numpy version error

--- job-posting/src/job_posting/crew.py
+++ job-posting/src/job_posting/crew.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel, Field
 web_search_tool = WebsiteSearchTool()
 seper_dev_tool = SerperDevTool()
 file_read_tool = FileReadTool(
-    file_path='job_description_example.md',
+    file_path='src/job_posting/job_description_example.md',
     description='A tool to read the job description example file.'
 )
 
@@ -92,5 +92,5 @@ class JobPostingCrew:
             agents=self.agents,  # Automatically created by the @agent decorator
             tasks=self.tasks,  # Automatically created by the @task decorator
             process=Process.sequential,
-            verbose=2,
+            verbose=True,
         )
